98cd6dc683620699ece88851b3530816a03742cc
olivia-js <liv.symonds@gmail.com>
2020-06-24 22:05:14 +0100

    My Django Girls app, first commit

---
+++ pythontips.py
@@ -0,0 +1,18 @@
+# .py file names
+# funtions
+def funcName():
+    one = 1
+    #content
+
+"""command line run -->    get into executable files directory
+                           use command  'python fileName.py'    """
+
+print('program successfully ran')
+
+# for loops
+y = ["", ""]
+for x in y:
+    do = "this"
+
+for x in range(0,10):
+    do = "this"
